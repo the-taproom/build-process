3d71e59de87aefbc673cd85e410b27e1e559fd4b
kickinbahk <kickinbahk@gmail.com>
2020-08-14 10:07:46 -0600

    update packages, add rollup config, fix gulpfile

--- build-process-files/gulpfile.js
+++ build-process-files/gulpfile.js
@@ -17,49 +17,39 @@ const noderesolve = require("rollup-plugin-node-resolve");
 const commonjs = require("rollup-plugin-commonjs");
 const source = require("vinyl-source-stream");
 const buffer = require("vinyl-buffer");
-
 let cssPlugins = [autoprefixer(), cssnano()];
-
 const directories = ["src/", "theme/"];
-
 /**
  * Checks repo for directory setup if it is Slate-based or
  * one of the two other common options
  */
 function repoDirectoryCheck() {
   let repoDirectory = "./";
-
-  directories.forEach(directory => {
+  directories.forEach((directory) => {
     try {
       fs.accessSync(directory, fs.constants.F_OK);
       repoDirectory = directory;
     } catch (error) {}
   });
-
   return repoDirectory;
 }
-
 const themeDirectory = repoDirectoryCheck();
 const scriptsDirectory = themeDirectory + "scripts/";
 const stylesDirectory = themeDirectory + "styles/";
-
-const scriptsEntryFiles = fs.readdirSync(scriptsDirectory).filter(file => {
+const scriptsEntryFiles = fs.readdirSync(scriptsDirectory).filter((file) => {
   if (file.indexOf(".") > -1) {
     return true;
   }
 });
-
-const stylesEntryFiles = fs.readdirSync(stylesDirectory).filter(file => {
+const stylesEntryFiles = fs.readdirSync(stylesDirectory).filter((file) => {
   if (file.indexOf(".") > -1) {
     return true;
   }
 });
-
-const stylesBuildTask = fileName => {
+const stylesBuildTask = (fileName) => {
   log("~~~~~~~~~~~~~~~~");
   log("Styles Compiling...");
   log("Compiling: " + fileName);
-
   return gulp
     .src(stylesDirectory + fileName)
     .pipe(sourcemaps.init())
@@ -86,12 +76,10 @@ const stylesBuildTask = fileName => {
       log(fileName + " built!");
     });
 };
-
 const jsBuildTask = function (fileName) {
   log("~~~~~~~~~~~~~~~~");
   log("JS Compiling...");
   log("Compiling: " + fileName);
-
   return rollup({
     input: scriptsDirectory + fileName,
     external: ["$", "jquery", "jQuery"],
@@ -107,11 +95,16 @@ const jsBuildTask = function (fileName) {
       }),
       commonjs(),
     ],
-    onwarn: warning => {
+    onwarn: (warning) => {
       //   This avoids a weird issue with some of the rollup plugins just spamming warnings
       return true;
     },
   })
+    .on("error", function (err) {
+      console.log(err.toString());
+
+      this.emit("end");
+    })
     .pipe(source(fileName.replace(".js", "") + ".min.js"))
     .pipe(buffer())
     .pipe(uglify({ mangle: { reserved: ["jQuery", "$"], keep_fnames: true } }))
@@ -129,12 +122,10 @@ const jsBuildTask = function (fileName) {
       log(fileName + " built!");
     });
 };
-
 gulp.task("css", async function () {
   log("+++++++++++++++");
   log("CSS Compiling...");
   log("Compiling: main.scss");
-
   return gulp
     .src(stylesDirectory + "main.scss")
     .pipe(sourcemaps.init())
@@ -162,9 +153,8 @@ gulp.task("css", async function () {
       log("main.css built!");
     });
 });
-
 gulp.task("css-modules", async function () {
-  return stylesEntryFiles.forEach(entryFile => {
+  return stylesEntryFiles.forEach((entryFile) => {
     if (
       entryFile.includes(".css") ||
       (entryFile.includes(".scss") && entryFile !== "main.scss")
@@ -173,22 +163,18 @@ gulp.task("css-modules", async function () {
     }
   });
 });
-
 gulp.task("javascript", async function () {
-  return scriptsEntryFiles.forEach(entryFile => {
+  return scriptsEntryFiles.forEach((entryFile) => {
     if (entryFile.includes(".js")) {
       jsBuildTask(entryFile);
     }
   });
 });
-
 gulp.task("test", function () {
   console.log("test test test");
   return true;
 });
-
 gulp.task("build", gulp.parallel("css", "css-modules", "javascript"));
-
 gulp.task("default", () => {
   gulp.watch(scriptsDirectory + "**/*.js", gulp.parallel("javascript"));
   gulp.watch("./styles/**/*.scss", gulp.parallel("css"));
